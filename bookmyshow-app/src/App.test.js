d4c261bb5dc54c2871eb4013b2f2824e2c3369d3
Mr. Rajesh <rajeshtutta9143@gmail.com>
2026-05-12 17:36:27 +0530

    Update App.test.js

--- bookmyshow-app/src/App.test.js
+++ bookmyshow-app/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders application heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const headingElement = screen.getByText(/Book My Show Application/i);
+
+  expect(headingElement).toBeInTheDocument();
 });
